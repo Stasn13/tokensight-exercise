9f6eebf40559aa0337996607bd546356fe113f7b
Stas <stasn139@gmail.com>
2025-01-14 11:39:08 +0000

    improve page and connect web3 provider

---
+++ frontend/src/components/providers/Web3Provider.tsx
@@ -0,0 +1,27 @@
+import { mainnet, sepolia} from 'wagmi/chains';
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, WagmiProvider } from 'wagmi';
+
+const chains = [sepolia, mainnet] as const;
+export const wagmiConfig = createConfig(
+  getDefaultConfig({
+    chains,
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+    appName: 'BankWatch',
+    appDescription: 'Issue attestation to proof that you are reliable borrower',
+  }),
+);
+
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
