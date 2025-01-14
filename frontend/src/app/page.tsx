9dad0cd0e4b5dc34e13bc26b0166bce9cea2672d
Stas <stasn139@gmail.com>
2025-01-14 14:21:25 +0000

    refactor structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,29 @@
 'use client'
+
 import Navigation from "@/components/Navigation";
 import { Web3Provider } from "@/components/providers/Web3Provider";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import SearchModule from "@/components/SearchModule";
 import { ConnectKitButton } from "connectkit";
 
-export default function Home() {
+export default function Home({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <Web3Provider>
-      <div className="absolute right-8 top-4 z-20">
-        <ConnectKitButton />
-      </div>
-      <div className="flex p-2 md:px-4 gap-2 md:gap-0 h-[100vh] overflow-hidden">
-        <Navigation />
-        <main
-          className="w-full px-0 md:px-8 overflow-y-auto pt-4 pb-12 max-w-[1200px] mx-auto"
-        >
-          <div className="flex w-full max-w-sm items-center space-x-2">
-            <Input type="search" placeholder="Fill with your token address" />
-            <Button type="submit">Subscribe</Button>
+    <div>
+      <Web3Provider>
+        <div className="absolute right-8 top-4 z-20">
+          <ConnectKitButton />
+        </div>
+        <div className="flex p-2 md:px-4 gap-2 md:gap-0 h-[100vh] overflow-hidden">
+          <Navigation />
+          <div
+            className="w-full px-0 md:px-8 overflow-y-auto pt-4 pb-12 max-w-[1200px] mx-auto"
+          >
+            <SearchModule/>
+            <main>
+              {children}
+            </main>
           </div>
-
-        </main>
-      </div>
-    </Web3Provider>
+        </div>
+      </Web3Provider>
+    </div>
   );
 }
--- frontend/src/components/providers/Web3Provider.tsx
+++ frontend/src/components/providers/Web3Provider.tsx
@@ -1,27 +1,29 @@
-import { mainnet, sepolia} from 'wagmi/chains';
+import { mainnet, sepolia } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createConfig, WagmiProvider } from 'wagmi';
 
 const chains = [sepolia, mainnet] as const;
-export const wagmiConfig = createConfig(
-  getDefaultConfig({
+const config = {
     chains,
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
     appName: 'BankWatch',
     appDescription: 'Issue attestation to proof that you are reliable borrower',
-  }),
+};
+export const wagmiConfig = createConfig(
+    getDefaultConfig(config),
 );
+
 
 
 const queryClient = new QueryClient();
 
-export const Web3Provider = ({ children }: {children: React.ReactNode}) => {
-  return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>{children}</ConnectKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <WagmiProvider config={wagmiConfig}>
+            <QueryClientProvider client={queryClient}>
+                <ConnectKitProvider>{children}</ConnectKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
 };
