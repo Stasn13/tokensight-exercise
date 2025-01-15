00b96b1c1c68ac85a40c0fafdb9bcef87fecb0b0
Stas <stasn139@gmail.com>
2025-01-15 11:21:35 +0000

    add chart and fix chart wrapper

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -23,7 +23,8 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    // p-6
+    className={cn("flex flex-col space-y-1.5 ", className)}
     {...props}
   />
 ))
@@ -57,7 +58,12 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div
+    // p-6
+    ref={ref}
+    className={cn(" pt-0", className)}
+    {...props}
+  />
 ))
 CardContent.displayName = "CardContent"
 
