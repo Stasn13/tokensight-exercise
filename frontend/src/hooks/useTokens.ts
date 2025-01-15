7e0ded42da037f27fef6a14eb2f025df4442e981
Stas <stasn139@gmail.com>
2025-01-15 12:00:36 +0000

    enhance tokens fetch and types

--- frontend/src/hooks/useTokens.ts
+++ frontend/src/hooks/useTokens.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from '@/lib/constants'
 import { useQuery } from '@tanstack/react-query'
-// todo: fix types
+
 type Token = {
     attributes: {
         name: string
@@ -14,21 +14,11 @@ type Token = {
     id: number
 }
 
-const fetchToken = async (searchValue: string): Promise<Token> => {
-    const id = "aave"; // todo: should change
-    const vsCurrency = "usd";
-    const days = 365;
-    const response = await fetch(`${baseUrl}/networks/eth/tokens/${searchValue}`)
-    const detailedfResponse = await fetch(`https://api.coingecko.com/api/v3/coins/eth/contract/${searchValue}`)
-    const tokenChart = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${vsCurrency}&days=${days}`)
+const fetchToken = async (addr: string): Promise<Token> => {
+    const response = await fetch(`${baseUrl}/networks/eth/tokens/${addr}`)
 
-    const chartData = await tokenChart.json();
-    const formatedChartData = chartData.prices.map(([timestamp, price]: [number, number]) => ({
-        date: new Date(timestamp).toLocaleDateString("en-US", { month: "short", day: "numeric" }),
-        price: parseFloat(price.toFixed(2))
-      }))
     const token = await response.json()
-    return formatedChartData
+    return token.data
 }
 
 const useToken = (searchValue: string) => {
