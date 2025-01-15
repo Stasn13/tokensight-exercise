485f6f3b0b708f313ff7b72dd8d19ab03c0ab862
Stas <stasn139@gmail.com>
2025-01-15 14:19:51 +0000

    enhance dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -12,18 +12,19 @@ import { useChart } from "@/hooks/useChartData"
 const Dashboard = () => {
     const [value, setValue] = useState<string>("");
     const { data } = useToken(value);
-    const { data: dataSecurity } = useSecurityData(value);
-    const { data: dataChart } = useChart(data?.attributes.coingecko_coin_id);
+    const { data: dataSecurity, isLoading: loadingSecurity } = useSecurityData(value);
+    const { data: dataChart, isLoading: loadingChart } = useChart(data?.attributes.coingecko_coin_id);
 
     return (
         <main>
             <SearchModule onValueSet={setValue} />
-            <TokenView
+            {data && <TokenView
                 commonData={data}
                 chartData={dataChart}
                 securityData={dataSecurity}
-            />
-            Currently supproted only for the Ethereum network
+                loadingSecurity={loadingSecurity}
+                loadingChart={loadingChart}
+            />}
         </main>
     )
 }
--- frontend/src/components/SearchModule.tsx
+++ frontend/src/components/SearchModule.tsx
@@ -1,51 +1,54 @@
 import { cn } from "@/lib/utils"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
-import { useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { baseUrl } from "@/lib/constants"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu"
+import { on } from "events"
+import { Token } from "@/hooks/useTokens"
 
-const SearchModule = ({ className }: { className?: string }) => {
+
+const SearchModule = ({ className, onValueSet }: { className?: string, onValueSet: Dispatch<SetStateAction<string>> }) => {
     const [value, setValue] = useState<string>("");
     const [isLoading, setIsLoading] = useState(false);
+    const [open, setOpen] = useState(false);
 
-    // TODO: move fetches outside ui component
-    const fetchTokens = (searchValue: string) => fetch(`${baseUrl}/networks/eth/tokens/${searchValue}`).then((res) => res.json())
-    const fetchPairs = (searchValue: string) => fetch(`${baseUrl}/search/pools?page=1&query=${searchValue}&network=eth`).then((res) => res.json())
+    const [token, setToken] = useState();
+    const [pairs, setPairs] = useState([]);
+
+
+    useEffect(() => {
+        console.log(token, pairs, token || pairs.length)
+        if (token || pairs.length) {
+            setOpen(true)
+            console.log("open")
+        }
+    }, [token, pairs]);
 
     const search = (searchValue: string) => {
-        Promise.all([fetchTokens(searchValue), fetchPairs(searchValue)]).then((values) => {
-            console.log(values);
-            setIsLoading(false);
-        });
+        onValueSet(searchValue)
     }
-    console.log(value)
+
     return (
-        <div className={cn(className, "flex w-full max-w-sm items-center space-x-2")}>
-            <div>
+        <>
+            <div className={cn(className, "flex w-full items-center space-x-2")}>
                 <Input
                     type="search"
                     placeholder="Fill with your token address"
                     onChange={(e) => setValue(e.target.value)}
+                    className="max-w-[480px]"
                 />
                 <Button
                     type="submit"
                     onClick={() => search(value)}
+                    className="w-[80px]"
                 >
                     Search
                 </Button>
             </div>
-            <DropdownMenu>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    <DropdownMenuItem>Billing</DropdownMenuItem>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem>Subscription</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
+            <p className="text-sm text-muted-foreground mt-1 ml-4">
+                *Currently supproted only for the Ethereum network
+            </p>
+        </>
     )
 }
 
